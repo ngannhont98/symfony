4b9e3b97c2cb740f773798189f9c0fbd36a96cd5
ngannhont98 <nganphan.300798@gmail.com>
2019-09-19 18:13:33 +0700

    add-list-detail book

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,59 +4,95 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
+    public $session;
+
+    function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/add_new" , name="add_new")
      */
     public function add_new(Request $request)
     {
-        $book = new Book();
+        if (isset($_GET['add_new'])) {
+            $book = new Book();
 
-        $form = $this->createFormBuilder($book)
-            ->add('bookCode', TextType::class)
-            ->add('bookName', TextType::class)
-            ->add('produce_date', DateType::class)
-            ->add('review', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Add new'])
-            ->getForm();
+            $book->setBookCode($_GET['bookCode']);
+            $book->setBookName($_GET['bookName']);
+            $book->setReview($_GET['review']);
+            $book->setProduceDate($_GET['produce_date']);
 
-        $form->handleRequest($request);
-
-        if( $form->isSubmitted() || $form->isValid() ){
-            $bookCode = $form['bookCode']->getData();
-            $bookName = $form['bookName']->getData();
-            $produce_date = $form['produce_date']->getData();
-            $review = $form['review']->getData();
-
-            $book->setBookCode($bookCode);
-            $book->setBookName($bookName);
-            $book->setProduceDate($produce_date);
-            $book->setReview($review);
-
-            $this->addFlash(
-                'notice',
-                'Added successfull'
-            );
+            $this->session->set($book->bookCode, $book);
+            return $this->redirectToRoute("list_book");
         }
-
-        return $this->render('book/add_new.twig', array(
-            'form' => $form->createView()
-        ));
+        return $this->render('book/add_new.twig');
     }
 
     /**
      * @Route("/" , name="list_book")
      */
-    public function list()
+    public function list_book()
+    {
+        $data['list'] = (sizeof($this->session->all()) > 0) ? $this->session->all() : null;
+        return $this->render('book/list_book.twig', $data);
+    }
+
+    /**
+     * @Route("/detail/{book_id}" , name="book_detail")
+     */
+    public function detail_book($book_id)
+    {
+        $data['detail'] = $this->session->get($book_id);
+        return $this->render('book/detail_book.twig', $data);
+    }
+
+    /**
+     * @Route("/clear" , name="clear")
+     */
+    public function clear()
+    {
+        $this->session->clear();
+        return new Response('eo');
+    }
+
+    /**
+     * @Route("/home" , name="home")
+     */
+    public function home()
     {
 
+        if (isset($_GET['add_new'])) {
+            $book = new Book();
 
+            $book->setBookCode($_GET['bookCode']);
+            $book->setBookName($_GET['bookName']);
+            $book->setReview($_GET['review']);
+            $book->setProduceDate($_GET['produce_date']);
+
+            $this->session->set($book->bookCode, $book);
+
+        }
+
+        $list = sizeof($this->session->all()) > 0 ? $this->session->all() : null;
+
+        $data = [
+            'list' => $list
+        ];
+        return $this->render('book/home.twig',$data);
+    }
+
+    public function show($data){
+        echo "<pre>";
+        print_r($data);
+        die;
     }
 }
